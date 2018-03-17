d57bc7b5b63353758f5bba8a2c543b6528ac4389
aldy colares <aldycolares@gmail.com>
2018-03-17 15:41:37 -0300

    removed trash

--- services/apptest.js
+++
@@ -1,8 +0,0 @@
-let http = require('http'),
-  app = require('../services/lapWebApi/config/app');
-
-http.createServer(app).listen(app.get('port'), function () {
-  console.log(new Date().toLocaleString());
-  console.log('Expess Server escutando na porta ' +
-    app.get('port'));
-});
